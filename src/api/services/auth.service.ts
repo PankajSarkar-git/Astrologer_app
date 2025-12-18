6a6574a164d93b0fd99f79313ec8bd6d524a0059
Pankaj Sarkar <pankajsarkarweb456@gmail.com>
2025-12-18 22:15:42 +0530

    useDeviceToken

--- src/api/services/auth.service.ts
+++ src/api/services/auth.service.ts
@@ -36,8 +36,8 @@ export const AuthService = {
     return res.data; // user object
   },
 
-  logout: async () => {
-    const res = await api.post('/auth/logout');
+  deviceToken: async (payload: string) => {
+    const res = await api.post('/api/v1/device-token/register', payload);
     return res.data;
   },
 };
